5eb3e95bd6c795471384ea752c81ef1063334d61
lorien <lgarzape@emeal.nttdata.com>
2025-06-21 00:36:53 +0200

    Added pokeapi cache 10 minutes of time to live

--- src/main/java/com/alea/pokeapi/service/impl/PokeapiCacheServiceImpl.java
+++ src/main/java/com/alea/pokeapi/service/impl/PokeapiCacheServiceImpl.java
@@ -3,6 +3,7 @@ package com.alea.pokeapi.service.impl;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.alea.pokeapi.integration.PokeapiFeignClient;
@@ -14,6 +15,11 @@ public class PokeapiCacheServiceImpl implements PokeapiCacheService {
 
   private final Map<String, Pokemon> pokemonCache = new ConcurrentHashMap<>();
 
+  @Value("${pokemonapi.cache.timeToLive}")
+  private long cacheTtlMilis;
+
+  private long cacheLiveTimeMilis = 0;
+
   private PokeapiFeignClient feignClient;
 
   public PokeapiCacheServiceImpl(PokeapiFeignClient feignClient) {
@@ -21,6 +27,8 @@ public class PokeapiCacheServiceImpl implements PokeapiCacheService {
   }
 
   public Pokemon getPokemonCache(String name) {
+    if(isExpired()) clean();
+
     return pokemonCache.computeIfAbsent(name, key -> {
       try {
         return feignClient.getPokemon(key);
@@ -28,5 +36,14 @@ public class PokeapiCacheServiceImpl implements PokeapiCacheService {
         return null;
       }
     });
+  }
+
+  private boolean isExpired() {
+    return System.currentTimeMillis() - cacheLiveTimeMilis > cacheTtlMilis;
+  }
+
+  private void clean() {
+    pokemonCache.clear();
+    cacheLiveTimeMilis = System.currentTimeMillis();
   }
 }
